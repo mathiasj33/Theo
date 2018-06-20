cc27f9c06a983163bf49e9ac783c86fa2f69a8d9
Mathias Jackermeier <mathias.jackermeier@tum.de>
2018-06-20 15:30:05 +0200

    Added the new template code for sheet 6

---
+++ src/blatt06/src/ToEpsilon.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public final class ToEpsilon {
+    private ToEpsilon() {
+    }
+
+    public static void main(String... args) throws IOException {
+        try (BufferedReader reader =
+                     new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8))) {
+            System.out.println(toEpsilonNFA(TuringMachine.parse(reader)));
+        }
+    }
+
+    public static <S> EpsilonNFA<?> toEpsilonNFA(TuringMachine<S> tm) {
+        // TODO
+    }
+}
