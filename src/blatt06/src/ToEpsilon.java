568f87624fb71dc11d03ad61519ab766b7ae5cdf
Mathias Jackermeier <mathias.jackermeier@outlook.de>
2018-06-23 17:16:24 +0200

    Finished epsilon approach, still wrong...

--- src/blatt06/src/ToEpsilon.java
+++ src/blatt06/src/ToEpsilon.java
@@ -18,25 +18,29 @@ public final class ToEpsilon {
     }
 
     public static <S> EpsilonNFA<?> toEpsilonNFA(TuringMachine<S> tm) {
-        EpsilonNFA<S> nfa = new EpsilonNFA<>(tm.getAlphabet(), tm.getInitialState());
-        tm.getStates().forEach(nfa::addState);
-        tm.getFinalStates().forEach(nfa::addFinalState);
+        EpsilonNFA<Pair<S, Boolean>> nfa = new EpsilonNFA<>(tm.getAlphabet(), new Pair<>(tm.getInitialState(), false));
+        tm.getStates().forEach(s -> {
+            nfa.addState(new Pair<>(s, false));
+            nfa.addState(new Pair<>(s, true));
+        });
+        tm.getFinalStates().forEach(s -> {
+            nfa.addFinalState(new Pair<>(s, false));
+            nfa.addFinalState(new Pair<>(s, true));
+        });
 
         for (Pair<S, TuringMachine.Transition<S>> pair : tm.getAllTransitions()) {
             if (pair.b.letter == TuringMachine.EMPTY_LETTER) {
-                //Determine if TM accepts this word after reading EMPTY_SYMBOL (a maximum of len(states) times)
-                if(Simulate.simulate(tm, "", tm.getStates().size()) == Simulate.Result.ACCEPTED) {
-                    nfa.addFinalState(pair.a);
-                }
+                nfa.addEpsilonTransition(new Pair<>(pair.a, false), new Pair<>(pair.b.successor, true));
+                nfa.addEpsilonTransition(new Pair<>(pair.a, true), new Pair<>(pair.b.successor, true));
             } else {
-                nfa.addTransition(pair.a, pair.b.letter, pair.b.successor);
+                nfa.addTransition(new Pair<>(pair.a, false), pair.b.letter, new Pair<>(pair.b.successor, false));
             }
         }
 
         //final states need to accept everything
         for (S s : tm.getFinalStates()) {
             for (char c : tm.getAlphabet()) {
-                nfa.addTransition(s, c, s);
+                nfa.addTransition(new Pair<>(s, false), c, new Pair<>(s, false));
             }
         }
 
